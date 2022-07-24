3551443ea3360552ffb4b285cec95bf3e1f70868
Haunted Bees <fench@hauntedbees.com>
2022-07-24 14:55:02 -0700

    don't need those anymore

--- Game/Weapons/Hammer.gd
+++
@@ -1,7 +0,0 @@
-extends Weapon
-
-func _ready():
-	attack_animation = "Melee"
-	cooldown = 0.2
-	attack_range = 3.0
-	pushback = 300.0
--- Game/Weapons/Pistol.gd
+++
@@ -1,7 +0,0 @@
-extends Weapon
-
-func _ready():
-	attack_animation = "Shoot"
-	cooldown = 0.3
-	attack_range = 20.0
-	pushback = 100.0
